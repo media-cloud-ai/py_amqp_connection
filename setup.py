41989a277d8b331be106252eb29816f24229dde7
Valentin NOEL <valent.noel@gmail.com>
2018-02-08 14:05:16 +0100

    Clean setup.py and add license description

--- setup.py
+++ setup.py
@@ -9,17 +9,18 @@ setup(
     name = 'amqp_connection',
     version = '1.0',
     description = 'Python AMQP connection for worker',
+    license='MIT',
     keywords = [
         'AMQP',
         'connection'
     ],
-    author='Marc-Antoine ARNAUD, Valentin NOEL',
-    author_email='valent.noel@gmail.com',
+    author = 'Marc-Antoine ARNAUD, Valentin NOEL',
+    author_email = 'valent.noel@gmail.com',
     url = 'https://github.com/FTV-Subtil/py_amqp_connection',
     packages = [
         'amqp_connection'
     ],
-    package_dir={
+    package_dir = {
         'amqp_connection': os.path.join(dir_name, 'src')
-    }
+    },
 )
