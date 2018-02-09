e309603556e4aab30ea67f3c8f655018956b2db6
Valentin NOEL <valent.noel@gmail.com>
2018-02-09 12:10:43 +0100

    Update package version to 1.1

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ dir_name = os.path.dirname(__file__)
 
 setup(
     name = 'amqp_connection',
-    version = '1.0',
+    version = '1.1',
     description = 'Python AMQP connection for worker',
     license='MIT',
     keywords = [
