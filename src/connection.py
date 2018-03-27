a9ce2be5196251375a324d382497e5563245d6bf
Valentin NOEL <valent.noel@gmail.com>
2018-03-27 15:49:29 +0200

    Separate consumer thread froom connection and message processing

--- src/connection.py
+++ src/connection.py
@@ -7,7 +7,7 @@ import logging
 
 from threading import Thread
 
-class ThreadWithReturnValue(Thread):
+class ConsumerProcessThread(Thread):
     """
     Thread that returns a value on join.
     """
@@ -19,28 +19,74 @@ class ThreadWithReturnValue(Thread):
         if self._target is not None:
             self._return = self._target(*self._args, **self._kwargs)
 
-    def join(self):
-        Thread.join(self)
+    def get_result(self):
         return self._return
 
-class BasicConsumer:
-    def __init__(self, callback, channel):
+    def join(self):
+        Thread.join(self)
+        return self.get_result()
+
+
+class BasicConsumer(Thread):
+
+    def __init__(self, callback, channel, group=None, target=None, name=None, args=(), kwargs=None, *, daemon=None):
+        Thread.__init__(self, group, target, name, args, kwargs, daemon=daemon)
         self.consumer_callback = callback
         self.channel = channel
+        self.current_delivery_tag = None
+        self.current_thread = None
+        self.start()
 
-    def callback(self, ch, method, properties, body):
-        ack = False
+    def is_processing(self):
+        return self.current_thread is not None and self.current_thread.is_alive()
+
+    def is_ready(self):
+        return self.current_thread is None and self.current_delivery_tag is None
+
+    def check_current_thread_result(self):
+        result = self.current_thread.join()
+
+        logging.debug("Check consumer thread '%s' result: %s", self.current_thread.name, "ACK" if result else "NACK")
+        if result in [None, True]:
+            self.channel.basic_ack(self.current_delivery_tag)
+        else:
+            self.channel.basic_nack(self.current_delivery_tag)
+
+        self.reset()
+
+    def run(self):
+        while True:
+            if not self.is_processing():
+                time.sleep(1)
+                continue
+            self.check_current_thread_result()
+
+    def reset(self):
+        logging.debug("Reset consumer thread parameters...")
+        self.current_thread = None
+        self.current_delivery_tag = None
+
+    def handle_message(self, ch, method, properties, body):
+        if self.is_processing():
+            # Already processing a message, NACK incoming messages
+            time.sleep(1)
+            self.channel.basic_nack(method.delivery_tag)
+            return
+
+        while not self.is_ready():
+            time.sleep(1)
+
+        # Start a new process thread
         try:
-            thread = ThreadWithReturnValue(target = self.consumer_callback.__call__, args = (ch, method, properties, body))
-            thread.start()
-            ack = thread.join()
+            self.current_thread = ConsumerProcessThread(target = self.consumer_callback.__call__,
+                name = "ConsumerProcessThread", args = (ch, method, properties, body))
+            logging.debug("Start new process thread: %s", self.current_thread)
+            self.current_thread.start()
+            self.current_delivery_tag = method.delivery_tag
+
         except Exception as e:
             logging.error("An error occurred in consumer callback: %s", e)
 
-        if ack in [None, True]:
-            self.channel.basic_ack(method.delivery_tag)
-        else:
-            self.channel.basic_nack(method.delivery_tag)
 
 class Connection:
 
@@ -90,8 +136,8 @@ class Connection:
         self.channel = channel
 
     def consume(self, queue, callback):
-        consumer = BasicConsumer(callback, self.channel)
-        self.channel.basic_consume(consumer.callback,
+        consumer = BasicConsumer(callback, self.channel, name = "BasicConsumer", daemon = True)
+        self.channel.basic_consume(consumer.handle_message,
                       queue=queue,
                       no_ack=False)
 
